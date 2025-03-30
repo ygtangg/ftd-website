07ac48f2a31e7f4dc07e86ff6f3eb9a5aa277008
Kelly Tang <ygtang@berkeley.edu>
2025-03-29 19:23:57 -0700

    start working on login

---
+++ app/login/page.tsx
@@ -0,0 +1,8 @@
+import { RegisterLink, LoginLink } from "@kinde-oss/kinde-auth-nextjs/components";
+// import { Button } from "@/components/ui/button";
+
+// export default function Login() {
+//     return (
+        
+//     )
+// }
