620f844ed127388b0c28b68124905c4334d75495
Kelly Tang <ygtang@berkeley.edu>
2025-08-21 16:41:58 -0700

    fixed footer

--- components/Footer.tsx
+++ components/Footer.tsx
@@ -4,8 +4,8 @@ import logo from "../public/image/ftd_logo.png";
 export const Footer = () => {
   return (
     <footer className="bg-lightpink opacity-85 text-black py-6 mb-0">
-      <div className="container mx-auto flex justify-center items-center">
-        <div className="flex flex-col items-center">
+      <div className="container mx-auto flex flex-col justify-center sm:justify-between items-center">
+        <div className="flex flex-col items-center sm:items-center sm:w-1/2">
           <h5 className="font-bold mb-2">Contact Us</h5>
           <div className="flex space-x-4 mb-2">
             <a
@@ -35,7 +35,7 @@ export const Footer = () => {
           </div>
           <div className="text-sm">&copy; {new Date().getFullYear()} FTD</div>
         </div>
-        <div className="absolute right-20 flex flex-col items-end">
+        <div className="mt-4 sm:mt-0 flex justify-center sm:justify-end w-full sm:w-auto">
           <Image src={logo} height={100} alt="logo" />
         </div>
       </div>
